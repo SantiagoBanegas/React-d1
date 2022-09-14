6fc965fb7c4952da6073ab940e0a8b370a4a00a5
Santiago Banegas <santibanegas88@gmai.com>
2022-09-13 23:44:06 -0300

    Segunda Entrega

--- Desktop/Programacion/React/proyecto1/src/App.js
+++ Desktop/Programacion/React/proyecto1/src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
+
 import './App.css';
+import NavBar from './Components/NavBar';
+
 
 function App() {
+  let titulo = "Hola Mundo";
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>{titulo}</h1>
       </header>
+      <NavBar />
     </div>
-  );
+  ); 
 }
 
 export default App;
